f4c137bbe26d3256a23924c55ceaa79162400208
bwettenstein <wettenstein@gmail.com>
2018-09-15 23:27:45 -0700

    First Commit
    First program with python

---
+++ Die Roll/randomNum.py
@@ -0,0 +1,32 @@
+import random;
+
+#Program is a loop that restarts if the user decides to run the program again
+#First program in python designed to learn loops, variables, and general syntax
+
+while True:
+    print("This program is designed to roll a die and return the value that is rolled \n")
+    min = int(input("Enter a min "));
+    if min <= 0:
+        print("The min must be greater than 0. Min will now be 1");
+        min = 1;
+    max = int(input("Enter a max "));
+    if max <= min:
+        print("The max value cannot be less than/equal to the min. Max is now 10");
+        max = 10;
+
+    result = random.randint(min, max);
+
+    print('Rolling die.... The result is ', result);
+
+    while True:
+        answer = input("Would you like to run again? (y/n)" );
+        if answer in('y', 'n'):
+            break;
+            print("Invalid Input");
+    if answer == 'y':
+        continue;
+    else:
+        print("Thank you for playing ");
+        break;
+
+
